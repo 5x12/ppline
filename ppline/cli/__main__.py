4fd5dd73b7d26e1820eace757e5cc49601a4d1da
Andrew Wolf <andrew.wolf@ru.ey.com>
2021-08-08 02:50:35 +0300

    pushing version 0.2.4

--- ppline/cli/__main__.py
+++ ppline/cli/__main__.py
@@ -20,12 +20,12 @@ def _arg_project_dir(arg):
 #     else:
 #         raise Exception(f'Argument {arg} must be an existing directory.')
 
-def _arg_trigger_class(arg):
-    expanded = os.path.expanduser(arg)
-    if os.path.isdir(expanded):
-        return os.path.abspath(os.path.normpath(expanded))
-    else:
-        raise Exception(f'Argument {arg} must be an existing directory.')
+# def _arg_trigger_class(arg):
+#     expanded = os.path.expanduser(arg)
+#     if os.path.isdir(expanded):
+#         return os.path.abspath(os.path.normpath(expanded))
+#     else:
+#         raise Exception(f'Argument {arg} must be an existing directory.')
 
 
 def _arg_config_file(arg):
@@ -37,9 +37,9 @@ def _arg_config_file(arg):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--trigger_class', '-tc', type=_arg_trigger_class, default=None, help='Path to class to trigger (in the form of "to/the/file.py:TestClass. TestClass should have a __call__ method").')
+parser.add_argument('--trigger_class', '-tc', default=None, help='Path to class to trigger (in the form of "to/the/file.py:TestClass. TestClass should have a __call__ method").')
 parser.add_argument('--project_dir', '-p', type=_arg_project_dir, default=None, help="Path to project's root where configuration .yml/.yaml file is stored.")
-parser.add_argument('--config_file', '-f', type=_arg_config_file, help='Name of pipeline configuration .yml/.yaml file.')
+parser.add_argument('--config_file', '-f', type=_arg_config_file, default=None, help='Name of pipeline configuration .yml/.yaml file.')
 
 # req_grp = parser.add_argument_group(title='Required')
 # req_grp.add_argument('--config_file', type=_arg_config_file, help='Name of pipeline config .yml file.', required=True)
